eb15253e13e543249212e8be86937f4dcae637f6
ishrak-liberatelabs <ishrak@liberate-labs.com>
2023-09-28 20:11:32 +0600

    About Section Added

--- src/Components/Home/About/About.jsx
+++ src/Components/Home/About/About.jsx
@@ -1,27 +1,38 @@
-
-
 const About = () => {
-    return (
-
-        <div className="hero min-h-screen bg-base-200">
-          <div className="hero-content flex-col lg:flex-row">
-            <img
-              src="/images/stock/photo-1635805737707-575885ab0820.jpg"
-              className="max-w-sm rounded-lg shadow-2xl"
-            />
-            <div>
-              <h1 className="text-5xl font-bold">Box Office News!</h1>
-              <p className="py-6">
-                Provident cupiditate voluptatem et in. Quaerat fugiat ut
-                assumenda excepturi exercitationem quasi. In deleniti eaque aut
-                repudiandae et a id nisi.
-              </p>
-              <button className="btn btn-primary">Get Started</button>
-            </div>
-          </div>
+  return (
+    <div className="hero min-h-screen bg-base-200 mb-10">
+      <div className="hero-content flex-col lg:flex-row">
+        <div className="relative mb-10 md:mb-0">
+          <img
+            src="../../../../public/assets/images/about_us/person.jpg"
+            className="w-3/4 rounded-lg"
+          ></img>
+          <img
+            src="../../../../public/assets/images/about_us/parts.jpg"
+            className="w-1/2 absolute right-5 top-1/2 rounded-lg border-white border-8"
+          ></img>
         </div>
-  
-    );
+        <div>
+          <h1 className="text-5xl font-bold text-red-500">About Us</h1>
+          <h3>We are qualified & of experience in this field</h3>
+          <p className="py-6">
+            There are many variations of passages of Lorem Ipsum available, but
+            the majority have suffered alteration in some form, by injected
+            humour, or randomised words which don&apos;t look even slightly
+            believable.
+          </p>
+          <p className="py-6">
+            the majority have suffered alteration in some form, by injected
+            humour, or randomised words which don&apos;t look even slightly
+            believable.
+          </p>
+          <button className="btn btn-outline btn-error text-white">
+            Discover More
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default About;
